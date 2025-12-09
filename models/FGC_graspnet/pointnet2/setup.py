ff90ebe2450d008c8b62e9c7f18afd0302d251f8
allml <zch511@qq.com>
2025-12-09 17:13:16 +0800

    Fix CUDA include paths for conda environment

--- models/FGC_graspnet/pointnet2/setup.py
+++ models/FGC_graspnet/pointnet2/setup.py
@@ -1,59 +1,28 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-# 
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
-import glob
 import os
-import site
+import glob
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
+CONDA_PREFIX = os.environ.get("CONDA_PREFIX", "")
+
+INCLUDE_DIRS = [
+    os.path.join(ROOT, "_ext_src", "include"),
+    os.path.join(CONDA_PREFIX, "lib/python3.10/site-packages/nvidia/cuda_runtime/include"),
+    os.path.join(CONDA_PREFIX, "targets/x86_64-linux/include"),
+    os.path.join(CONDA_PREFIX, "include"),
+]
+
+LIB_DIRS = [
+    os.path.join(CONDA_PREFIX, "lib"),
+    os.path.join(CONDA_PREFIX, "lib64"),
+]
+
+print("Using include dirs:", INCLUDE_DIRS)
+print("Using library dirs:", LIB_DIRS)
 
 _ext_src_root = "_ext_src"
-_ext_sources = glob.glob("{}/src/*.cpp".format(_ext_src_root)) + glob.glob(
-    "{}/src/*.cu".format(_ext_src_root)
-)
-_ext_headers = glob.glob("{}/include/*".format(_ext_src_root))
-
-# 获取 conda 环境中的 CUDA 头文件路径
-def get_cuda_include_paths():
-    """查找 conda 环境中的 CUDA 头文件路径"""
-    include_paths = []
-    
-    # 获取 site-packages 路径
-    site_packages = site.getsitepackages()
-    if not site_packages:
-        # 如果没有找到，尝试使用当前环境的 site-packages
-        import sys
-        site_packages = [f for f in sys.path if 'site-packages' in f]
-    
-    for sp in site_packages:
-        # 查找 nvidia/cuda_runtime/include
-        nvidia_cuda_path = os.path.join(sp, "nvidia", "cuda_runtime", "include")
-        if os.path.exists(nvidia_cuda_path):
-            include_paths.append(nvidia_cuda_path)
-        
-        # 查找 triton 的 CUDA 头文件
-        triton_cuda_path = os.path.join(sp, "triton", "backends", "nvidia", "include")
-        if os.path.exists(triton_cuda_path):
-            include_paths.append(triton_cuda_path)
-    
-    return include_paths
-
-# 添加 CUDA 头文件路径到编译参数
-cuda_include_paths = get_cuda_include_paths()
-local_include = os.path.join(ROOT, _ext_src_root, "include")
-
-# 构建编译参数
-extra_compile_args_cxx = ["-O2", "-I{}".format(local_include)]
-extra_compile_args_nvcc = ["-O2", "-I{}".format(local_include)]
-
-# 添加 CUDA 头文件路径
-for path in cuda_include_paths:
-    extra_compile_args_cxx.append("-I{}".format(path))
-    extra_compile_args_nvcc.append("-I{}".format(path))
+_ext_sources = glob.glob(f"{_ext_src_root}/src/*.cpp") + glob.glob(f"{_ext_src_root}/src/*.cu")
 
 setup(
     name='pointnet2',
@@ -61,13 +30,13 @@ setup(
         CUDAExtension(
             name='pointnet2._ext',
             sources=_ext_sources,
+            include_dirs=INCLUDE_DIRS,
+            library_dirs=LIB_DIRS,
             extra_compile_args={
-                "cxx": extra_compile_args_cxx,
-                "nvcc": extra_compile_args_nvcc,
-            },
-        )
+                "cxx": ["-O2"],
+                "nvcc": ["-O2", "--expt-relaxed-constexpr"]
+            }
+        ),
     ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    cmdclass={'build_ext': BuildExtension}
 )
